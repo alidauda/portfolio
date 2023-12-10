fa4d62dee0bf760ebb1cc3c13c8d2f36d1abfac9
alidauda <alidauda14@gmail.com>
2023-12-10 01:31:37 +0100

    first commit

---
+++ src/app/ui/Heading/Heading.tsx
@@ -0,0 +1,17 @@
+export default function Heading() {
+  return (
+    <div className='py-7 border-b-2'>
+      <p className='text-justify'>
+        Hey there, I&apos;m <span className='font-bold text-lg'>Ali</span>—a
+        software developer who finds joy in coding and exploring the fascinating
+        world of programming. But beyond the screens and algorithms,
+        there&apos;s another world I adore—the kitchen.{' '}
+        <span className='font-bold text-lg'>Cooking</span> isn&apos;t just a
+        hobby; it&apos;s my other passion, where I play with flavors and
+        ingredients to create delicious dishes. Here, I blend my love for coding
+        with my love for <span className='font-bold text-lg'>cooking</span>.
+        Join me as I share my journey through these two wonderful realms!
+      </p>
+    </div>
+  );
+}
---
+++ src/app/ui/NavBar/nav.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+import { ChefHat } from 'lucide-react';
+
+export default function NavBar() {
+  return (
+    <div className='flex justify-between py-5'>
+      <ul className='flex space-x-5  '>
+        <li>
+          <Link href='/'>Home</Link>
+        </li>
+        <li>
+          <Link href='#'> blog</Link>
+        </li>
+        <li>
+          {' '}
+          <Link href='#'> Cook </Link>{' '}
+        </li>
+      </ul>
+      <div>
+        {' '}
+        <Link href='/'>
+          <ChefHat />
+        </Link>
+      </div>
+    </div>
+  );
+}
