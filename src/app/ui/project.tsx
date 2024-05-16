459e2a6e1c75a24d4e7b526632d0923d8a65f24a
alidauda <alidauda14@gmail.com>
2024-05-16 21:51:05 +0100

    added exhcain

--- src/app/ui/project.tsx
+++ src/app/ui/project.tsx
@@ -1,29 +1,27 @@
-import Link from 'next/link';
-import { CSSProperties } from 'react';
-import Image from 'next/image';
-import { Github } from 'lucide-react';
+import Link from "next/link";
+import { CSSProperties } from "react";
+import Image from "next/image";
+import { Github } from "lucide-react";
 
 const projects = [
   {
-    title: 'Chat with Pdf using OpenAi and LangChain',
+    title: "Exchain",
     description:
-      'developed using Next.js, TypeScript, Prisma, LangChain, OpenAI, UploadThing, and PlanetScale for the database.',
-    link: 'https://ask-gpt-green.vercel.app/',
-    github: 'https://github.com/alidauda/ask-gpt',
+      "A crypto exchange platform developed using Next.js, TypeScript, TailwindCSS, and Vercel for deployment.",
+    link: "https://ui-test-xi.vercel.app/",
+  },
+
+  {
+    title: "DashBaord for a delivery company",
+    description:
+      "developed using Next.js, TypeScript, TailwindCSS, and Vercel for deployment.",
+    link: "https://btcexpress.vercel.app/",
   },
   {
-    title: 'My Portfolio',
+    title: "Chat with Pdf using OpenAi and LangChain",
     description:
-      'developed using Next.js, TypeScript, TailwindCSS, and Vercel for deployment.',
-    link: '/',
-    github: 'https://github.com/alidauda/portfolio',
-  },
-  {
-    title: 'DashBaord for a delivery company',
-    description:
-      'developed using Next.js, TypeScript, TailwindCSS, and Vercel for deployment.',
-    link: 'https://btcexpress.vercel.app/',
-    github: '#',
+      "developed using Next.js, TypeScript, Prisma, LangChain, OpenAI, UploadThing, and PlanetScale for the database.",
+    link: "https://ask-gpt-green.vercel.app/",
   },
 ];
 
@@ -35,12 +33,12 @@ export default function Project() {
           key={project.description}
           style={
             {
-              '--background': '30 41 59',
-              '--highlight': '255 255 255',
+              "--background": "30 41 59",
+              "--highlight": "255 255 255",
 
-              '--bg-color':
-                'linear-gradient(rgb(var(--background)), rgb(var(--background)))',
-              '--border-color': `linear-gradient(145deg,
+              "--bg-color":
+                "linear-gradient(rgb(var(--background)), rgb(var(--background)))",
+              "--border-color": `linear-gradient(145deg,
         rgb(var(--highlight)) 0%,
         rgb(var(--highlight) / 0.3) 33.33%,
         rgb(var(--highlight) / 0.14) 66.67%,
@@ -48,20 +46,16 @@ export default function Project() {
       `,
             } as CSSProperties
           }
-          className='flex     flex-col  rounded-xl border border-transparent  p-2
+          className="flex     flex-col  rounded-xl border border-transparent  p-2
   
-  hover:[background:padding-box_var(--bg-color),border-box_var(--border-color)]'
+  hover:[background:padding-box_var(--bg-color),border-box_var(--border-color)]"
         >
           <Link href={project.link}>
-            <h3 className='text-xl font-medium text-white underline'>
+            <h3 className="text-xl font-medium text-white underline">
               {project.title}
             </h3>
           </Link>
-          <p className=''>{project.description}</p>
-
-          <Link href={project.github} target='_blank'>
-            <Github />
-          </Link>
+          <p className="">{project.description}</p>
         </div>
       ))}
     </>
